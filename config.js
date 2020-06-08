617db5be091e624162babe71a8502e72990b1115
alkadeep neha <alkadeepneha@gmail.com>
2020-06-08 13:49:58 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     secretKey: "12345-67890-09876-54321",
     mongoUrl: "mongodb://localhost:27017/conFusion",
+    facebook: {
+        clientId: "181490879945015",
+        clientSecret: "6bb66136af855aefa6d2fdce15e1f763",
+    },
 };
